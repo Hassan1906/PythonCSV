cc1963d3839c2a112887fb3853659815260d4504
Hassan <zamhas1906@gmail.com>
2021-04-23 10:10:49 +0200

    Fixed Add row

--- CSVpandasAddRow.py
+++ CSVpandasAddRow.py
@@ -2,7 +2,7 @@ import pandas
 
 df = pandas.read_csv('hrdata_modified.csv')
 
-newEmployee = ['Hassan Zaman','2014-07-23',61000.00,5,'00-000-0000']
+newEmployee = ['Eren Jaeger','2015-08-23',90000.00,5,'00-000-0000']
 
 df.loc[len(df.index)+1] = newEmployee
 
